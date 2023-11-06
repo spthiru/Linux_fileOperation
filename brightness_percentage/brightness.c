0a3316f81cfef00ce2e028277697837cdb9b0a92
spthiru <126955622+spthiru@users.noreply.github.com>
2023-11-06 14:01:19 +0530

    Update brightness.c

--- brightness_percentage/brightness.c
+++ brightness_percentage/brightness.c
@@ -25,17 +25,42 @@ int main()
   //open source file
   //int open(const char *pathname, int flags);
   fd = open(BACK_LIGHT, openFlag);
-
+  if(fd == -1)
+  {
+    perror("error opening file");
+    exit(-1);
+  } 
   //ssize_t read(int fd, void *buf, size_t count);
   numRead = read(fd, buf, 255);
-
+  if(numRead == -1)
+  {
+    perror("error reading file");
+    close(fd);
+    exit(-2);
+  }
   //ssize_t write(int fd, const void *buf, size_t count);
   wt = write(1, buf1,50);
+  if(wt == -1)
+  {
+    perror("error writing a file");
+    close(numRead);
+    close(fd);
+    exit(-3);
+  }
 
   //ssize_t write(int fd, const void *buf, size_t count);
   wt1 = write(1, buf,numRead);
+  if(wt1 == -1)
+  {
+    perror("error writing a file");
+    close(wt);
+    close(numRead);
+    close(fd);
+    exit(-4);
+  }
 
   //int close(int fd);
   close(fd);
+  close(wt1);
   exit(0);
 }
