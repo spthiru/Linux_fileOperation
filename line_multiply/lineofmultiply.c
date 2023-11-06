f1c3d2d776634ea521c38b9194ebbb8f552f5815
spthiru <126955622+spthiru@users.noreply.github.com>
2023-11-06 14:12:03 +0530

    Update lineofmultiply.c

--- line_multiply/lineofmultiply.c
+++ line_multiply/lineofmultiply.c
@@ -1,3 +1,4 @@
+//header file incluction
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -5,7 +6,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+//macro
 #define BUF_SIZE 100000
+
+//main function
 int main(int argc, char *argv[])
 {
   int fd , fd1, openFlag,openFlag1, newLine=0, lineNum, bytes=0, bytes2=0, inc_val=1, bytesAdd=0;
@@ -18,29 +22,33 @@ int main(int argc, char *argv[])
   char buf[BUF_SIZE];
   char buf1[BUF_SIZE];
 
-  openFlag = O_RDONLY;
-  openFlag1 = O_CREAT | O_WRONLY;
+  openFlag = O_RDONLY;//openflag for source file opening
+  openFlag1 = O_CREAT | O_WRONLY; //open flag for output creat file
 
-  filePerm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH; 
+  filePerm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH; //file permission of output fle creating
 
   //int open(const char *pathname, int flags);
   fd = open(argv[1], openFlag);
+  if(fd == -1)
+  {
+	  
+  }
 
   //int open(const char *pathname, int flags);
   fd1 = open(argv[2], openFlag1, filePerm);
   if(fd == -1)
   {
-  perror("open()");
-  close(fd);
+  	perror("open()");
+  	close(fd);
   }
 
   //ssize_t read(int fd, void *buf, size_t count);
   readBytes = read(fd, buf, BUF_SIZE);
   if(readBytes == -1)
   {
-    perror("read()");
-    close(fd);
-    close(fd1);
+    	perror("read()");
+    	close(fd);
+    	close(fd1);
   }
 
   for(int i=0; i<readBytes; i++ )
