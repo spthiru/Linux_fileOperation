1225983ae1d96ca2f0f0836d615740f0e01240fb
spthiru <126955622+spthiru@users.noreply.github.com>
2023-11-10 17:03:21 +0530

    Add files via upload

--- line_multiply/lineofmultiply.c
+++ line_multiply/lineofmultiply.c
@@ -1,4 +1,3 @@
-//header file incluction
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -8,8 +7,6 @@
 
 //macro
 #define BUF_SIZE 100000
-
-//main function
 int main(int argc, char *argv[])
 {
   int fd , fd1, openFlag,openFlag1, newLine=0, lineNum, bytes=0, bytes2=0, inc_val=1, bytesAdd=0;
@@ -21,72 +18,71 @@ int main(int argc, char *argv[])
   off_t curSet1;
   char buf[BUF_SIZE];
   char buf1[BUF_SIZE];
+  int ret = EXIT_SUCCESS;
 
-  openFlag = O_RDONLY;//openflag for source file opening
-  openFlag1 = O_CREAT | O_WRONLY; //open flag for output creat file
+  openFlag = O_RDONLY;
+  openFlag1 = O_CREAT | O_WRONLY;
 
-  filePerm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH; //file permission of output fle creating
+  filePerm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH; 
 
+  //open a content file
   //int open(const char *pathname, int flags);
   fd = open(argv[1], openFlag);
   if(fd == -1)
   {
-     perror("error opening source_file");
-     exit(-1);
+     ret = errno;
+     perror("error opening input file");
+     goto exit_ret;
   }
 
+  //creating new file in output
   //int open(const char *pathname, int flags);
   fd1 = open(argv[2], openFlag1, filePerm);
   if(fd1 == -1)
   {
-     perror("error opening creating output file");
-     close(fd);
-     exit(-2);
+     ret = errno;
+     perror("Error cannot create newFile");
+     goto close_fd;
   }
-
+  //Reading input file
   //ssize_t read(int fd, void *buf, size_t count);
   readBytes = read(fd, buf, BUF_SIZE);
   if(readBytes == -1)
-  { 
-     perror("error reading source file");
-     close(fd);
-     close(fd1);
-     exit(-3);
+  {
+    ret = errno;
+    perror("Error cannot read input file:");
+    goto close_fd1;
   }
 
   for(int i=0; i<readBytes; i++ )
   {
-    bytes++; //calculating bytes
+    bytes++;
     if(buf[i] == 10)
     {
-      newLine++; //calculating new lines
+      newLine++;
     }
 
-    lineNum =(10*inc_val)-1; //calculation which line  multiply writng in output file
+    lineNum =(10*inc_val)-1;
 
     if(newLine == lineNum )
     {
+      //set cursur to 10th line in input file
       //off_t lseek(int fd, off_t offset, int whence);
       curSet = lseek(fd, bytes, SEEK_SET);
       if(curSet == -1)
       {
-	perror("error set curser of source file");
-	close(readBytes);
-        close(fd1);
-        close(fd);
-        exit(-4);
+	 ret = errno;
+	 perror("cannot set a cursur in input file:");
+	 goto close_fd1;
       }
-
+      //reading output file for set cursur
       //ssize_t read(int fd, void *buf, size_t count);
       readBytes1 = read(fd, buf1, BUF_SIZE);
       if(readBytes1 == -1)
       {
-	perror("error reading source file");
-	close(curSet);
-	close(readBytes);
-	close(fd1);
-	close(fd);
-	exit(-5);
+	 ret = errno;
+	 perror("Error reading output file");
+	 goto close_fd1;
       }
 
       for(int j=0; j < readBytes1; j++)
@@ -97,34 +93,26 @@ int main(int argc, char *argv[])
 	  break;
 	}
       }
-
+      //write a data to output file
       //ssize_t write(int fd, const void *buf, size_t count);
       writebytes = write(fd1, buf1, bytes2);
-      if(writebytes == -1)
+      if(writeBytes == -1)
       {
-	perror("error writing a output file");
-	close(readBytes1);
-	close(curSet);
-	close(readBytes);
-	close(fd1);
-	close(fd);
-	exit(-5);
-	      
+	 ret = errno;
+	 perror("error writing to output file");
+	 goto close_fd1;
       }
+
       bytesAdd = bytesAdd + bytes2;
 
+      //set cursur output file
       //off_t lseek(int fd, off_t offset, int whence);
       curSet1 = lseek(fd1, bytesAdd, SEEK_SET);
       if(curSet1 == -1)
       {
-	perror("error set curser a output file");
-	close(writeBytes);
-	close(readBytes1);
-	close(curSet);
-	close(readBytes);
-	close(fd1);
-	close(fd);
-	exit(-6);
+	 ret = errno;
+	 perror("Error setcur for ouput file");
+	 goto close_fd1;
       }
 
       bytes2 = 0;
@@ -132,8 +120,11 @@ int main(int argc, char *argv[])
       inc_val++;
     }
   }
-
-  // int close(int fd);
-  close(fd);
-  close(fd1);
+exit_ret:
+  	exit(-ret);
+close_fd:
+	close(fd);
+close_fd1:
+	close(fd);
+	close(fd1);
 }
