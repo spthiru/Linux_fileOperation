d3b0f012587c41351926142d3c18d76579660830
spthiru <126955622+spthiru@users.noreply.github.com>
2023-11-10 15:57:41 +0530

    Add files via upload

--- mouse_reading/mice.c
+++ mouse_reading/mice.c
@@ -1,6 +1,7 @@
 #include <sys/types.h>
 #include <linux/input.h>
 #include <sys/stat.h>
+#include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -12,41 +13,53 @@ int main()
   int fd,openFlag;
   ssize_t readFile;
   ssize_t writeFile;
-  struct input_event event ;
+  struct input_event event;
+  int ret = EXIT_SUCCESS;
 
   openFlag = O_RDONLY; 
 
+  //open mouse file
   //int open(const char *pathname, int flags);
   fd = open(MOUSEFILE, openFlag);
   if(fd == -1)
   {
-    perror("open()");
+    ret = errno;
+    perror("mice:cannot open mouse file ");
+    goto exit_ret;
   }
-  else
-  {
+   //infinite loop
     while(1)
     {
+       //reading mouse file
       //ssize_t read(int fd, void *buf, size_t count);
       readFile = read (fd, &event, sizeof(event));
       if(readFile == -1)
       {
-	perror("read()");
+	 ret = errno;
+	 perror("mice:cannot reading the mouse file");
+	 goto close_fd;
       }
-      else
+      //writing mouse file
+      //ssize_t write(int fd, const void *buf, size_t count);
+      writeFile = write(1, &event, readFile);
+      if(writeFile  == -1)
       {
-	//ssize_t write(int fd, const void *buf, size_t count);
-	writeFile = write(1, &event, readFile);
-
-	if(event.code == 272)
-	{
-	  printf("left Button\n");
-	}
-	printf("%x %x %x %x\n",event.value,event.code,event.time,event.type);
-	if(event.code == 273)
-	{
-	  printf("Right Button\n");
-	}
+	 ret = errno;
+	 perror("Error cannot writing mouse file");
+	 goto close_fd;
+      }
+      if(event.code == 272)
+      {
+	 printf("left Button\n");
+      }
+      printf("%x %x %x %x\n",event.value,event.code,event.time,event.type);
+      if(event.code == 273)
+      {
+	 printf("Right Button\n");
       }
     }
-  }
+exit_ret:
+	  exit(-ret);
+close_fd:
+	  close(fd);
 }
