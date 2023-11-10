1f8283179776906df47b040730193b78c814d2d5
spthiru <126955622+spthiru@users.noreply.github.com>
2023-11-10 15:26:57 +0530

    Add files via upload

--- brightness_percentage/brightness.c
+++ brightness_percentage/brightness.c
@@ -1,4 +1,10 @@
-//header file incuction
+/***************************************************************************************************************/
+/*   Author         : Thirumoorthy P                                                                           */
+/*   Date           : 7/11/2023                                                                                */
+/*   File name      : brightness.c                                                                             */
+/*   Description    : find a current brightness in our system                                                  */
+/***************************************************************************************************************/
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -7,10 +13,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-// macro
+//macro
 #define BACK_LIGHT "/sys/class/backlight/amdgpu_bl0/actual_brightness"
 
-//main function
 int main()
 {
   int fd, openFlag;
@@ -19,48 +24,48 @@ int main()
   ssize_t wt1;
   char buf1[50]="Brightness : ";
   char buf[255];
+  int ret = EXIT_SUCCESS;
 
-  openFlag = O_RDONLY;//open flag for source file
+  openFlag = O_RDONLY;
 
-  //open source file
+  //open brigtness file 
   //int open(const char *pathname, int flags);
   fd = open(BACK_LIGHT, openFlag);
   if(fd == -1)
   {
-    perror("error opening input file");
-    exit(-1);
-  } 
+     ret = errno;
+     perror("error opening brightness file :");
+     goto exit_ret;
+  }
+  //reading brightness %
   //ssize_t read(int fd, void *buf, size_t count);
   numRead = read(fd, buf, 255);
   if(numRead == -1)
   {
-    perror("error reading input file");
-    close(fd);
-    exit(-2);
+     ret =errno;
+     perror("error reading brightness file :");
+     goto close_fd;
   }
+  //writing to STDOUT
   //ssize_t write(int fd, const void *buf, size_t count);
   wt = write(1, buf1,50);
   if(wt == -1)
   {
-    perror("error writing buffer1");
-    close(numRead);
-    close(fd);
-    exit(-3);
+     ret = errno;
+     perror("error writing string to STDOUT");
+     goto close_fd;
   }
-
+  //write to STDOUT
   //ssize_t write(int fd, const void *buf, size_t count);
   wt1 = write(1, buf,numRead);
-  if(wt1 == -1)
+  if(wt1 ==-1)
   {
-    perror("error writing a input file");
-    close(wt);
-    close(numRead);
-    close(fd);
-    exit(-4);
+     ret = errno;
+     perror("error writing brightness file percentage:");
+     goto close_fd;
   }
-
-  //int close(int fd);
+exit_ret:
+  exit(-ret);
+close_fd:
   close(fd);
-  close(wt1);
-  exit(0);
 }
