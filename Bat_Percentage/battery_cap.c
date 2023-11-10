62be3e05b87426e64634636d65657d08cb741c5d
spthiru <126955622+spthiru@users.noreply.github.com>
2023-11-10 11:10:10 +0530

    Add files via upload

--- Bat_Percentage/battery_cap.c
+++ Bat_Percentage/battery_cap.c
@@ -1,3 +1,9 @@
+/***************************************************************************************************************/
+/*   Author         : Thirumoorthy P                                                                           */
+/*   Date           : 7/11/2023                                                                                */
+/*   File name      : bat_per.c                                                                                */
+/*   Description    : find a current battery percentage                                                        */
+/***************************************************************************************************************/
 //header file incluction
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -9,6 +15,7 @@
 
 //macro
 #define BUF_SIZE 50
+#define BAT_CAP_FILE_PATH "/sys/class/power_supply/BAT1/capacity"
 
 //main function
 int main(int argc, char *argv[])
@@ -19,51 +26,52 @@ int main(int argc, char *argv[])
   ssize_t writeFile1;
   char buf1[BUF_SIZE]="Battery Percentage:";
   char buf[BUF_SIZE];
+  int ret = EXIT_SUCCESS;
 
 
-  openFlag = O_RDONLY; //set openflag for sourcefile
+  openFlag = O_RDONLY; //set openflag for battery capacity file
 
-  //open source file
+  //open the battery capacity file
   // int open(const char *pathname, int flags);
-  fd = open("/sys/class/power_supply/BAT1/capacity", openFlag);
+  fd = open(BAT_CAP_FILE_PATH, openFlag);
   if(fd == -1)
   {
-    perror("error opening input file");
-    exit(-1);
+     ret = errno;
+    perror("error opening battery capacity file:");
+    goto exit_ret;
   }
 
+  //reading the battery capacity percentage
   //ssize_t read(int fd, void *buf, size_t count);
   numRead = read(fd, buf, BUF_SIZE);
   if(numRead == -1)
   {
-    perror("error reading input file\n");
-    close(fd);
-    exit(-2);
+     ret = errno;
+    perror("error reading battery capcity file:");
+    goto close_fd;
   }
 
+  //writing to STDOUT
   //ssize_t write(int fd, const void *buf, size_t count);
   writeFile = write(1, buf1, BUF_SIZE);
   if(writeFile == -1)
   {
-    perror("error writing buffer1\n");
-    close(numRead);
-    close(fd);
-    exit(-3);
+    ret = errno;
+    perror("error writing to STDOUT:");
+    goto close_fd;
   }
 
+  //writing to STDOUT
   //ssize_t write(int fd, const void *buf, size_t count);
   writeFile1 = write(1, buf, numRead);
   if(writeFile1 == -1)
   {
-    perror("error writing input file");
-    close(numRead);
-    close(writeFile);
-    close(fd);
-    exit(-5);
+     ret = errno; 
+    perror("error writing battery capacity percentage:");
   }
 
-  // int close(int fd);
-  close(writeFile1);
+close_fd:
   close(fd);
-  exit(0);
+exit_ret:
+  exit(-ret);
 }
