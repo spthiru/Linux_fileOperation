05099141c5552d2c9bbae1e4994ca668a61d7b66
spthiru <pthirumoorthy02062000@gmail.com>
2023-11-14 16:17:06 +0530

    modified chmod

--- chmod/chmod_syscall.c
+++ chmod/chmod_syscall.c
@@ -4,6 +4,7 @@
 /*   File name      : chmod_syscall.c                                                                          */
 /*   Description    : change the file permission                                                               */
 /***************************************************************************************************************/
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <errno.h>
@@ -13,20 +14,37 @@
 
 int main(int argc, char *argv[])
 {
-  int  ret_chmod, ret;
-  mode_t filePerm;
-
+  int  ret_chmod, ret, deci, div, mod, octal, temp=0, l=0;
+  int arr[8]={00,01,02,03,04,05,06,07};
+  //string to decimal converting
+  deci = atoi(argv[2]);
+  //decimal to octal converting
+  while(deci > 0)
+  {
+     div = deci / 10;
+     mod = deci % 10;
+     deci = div;
+     if(mod<=7 && mod>=0)
+     {
+	octal = arr[mod]<<l | temp;
+     }
+     else 
+     {
+	printf("Enter the vaild octal number\n");
+	exit(-1);
+     }
+     temp = octal;
+     l = l + 3;
+  }
   ret = EXIT_SUCCESS;
-  //file permission for input file
-  filePerm = S_IRWXU | S_IRWXG | S_IRWXO;
 
-  //change file permission
+  //change the file permission 
   //int chmod(const char *pathname, mode_t mode); 
-  ret_chmod = chmod(argv[1], filePerm);
+  ret_chmod = chmod(argv[1], octal);
   if(ret_chmod == -1)
   {
     ret = errno;
-    perror("error changing input file permission:");
+    perror("chmod: cannot change file permission");
     exit(-ret);
   }
 }
